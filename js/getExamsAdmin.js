f6911aa454ee0885c23a136e6045aae774b7ee65
m.a.sh3lan <m.a.sh3lan@gmail.com>
2015-12-22 15:36:09 -0500

    m

--- js/getExamsAdmin.js
+++ js/getExamsAdmin.js
@@ -1,30 +1,30 @@
-function getExams()
-{
-  alert("Hi");
-  $.get("/php/getExams.php",
-  function( data ) {
-    for(int i = 1 ; data.length ; i++)
-      displayExam(data[i].ETITLE , i);
-  },
-   "json"
-  );
-}
+// function getExams()
+// {
+//   alert("Hi");
+//   $.get("/php/getExams.php",
+//   function( data ) {
+//     for(var i = 1 ; data.length ; i++)
+//       displayExam(data[i].ETITLE , i);
+//   },
+//    "json"
+//   );
+// }
 
-function displayExam(examName , id)
-{
-   examHTML = '<div class="exam-item" id = "exam-"'+id+' >';
-   examHTML += '<div class = "row">';
-   examHTML += '<div class="col-md-8">';
-   examHTML += '<h3>'+ examName +'</h3>';
-   examHTML += '</div>';
-   examHTML += '<div class="col-md-2">';
-   examHTML += '<button id = "edit-'+id+'" type="button" class="btn btn-primary" onclick = "editExam(this)">Edit</button>';
-   examHTML += '</div>';
-   examHTML += '<div class="col-md-2">';
-   examHTML += '<button id = "delete-'+id+'" type="button" class="btn btn-danger" onclick = "deleteExam(this)">Delete</button>';
-   examHTML += '</div>';
-   examHTML += '</div>';
-   examHTML += '</div>';
+// function displayExam(examName , id)
+// {
+//    examHTML = '<div class="exam-item" id = "exam-"'+id+' >';
+//    examHTML += '<div class = "row">';
+//    examHTML += '<div class="col-md-8">';
+//    examHTML += '<h3>'+ examName +'</h3>';
+//    examHTML += '</div>';
+//    examHTML += '<div class="col-md-2">';
+//    examHTML += '<button id = "edit-'+id+'" type="button" class="btn btn-primary" onclick = "editExam(this)">Edit</button>';
+//    examHTML += '</div>';
+//    examHTML += '<div class="col-md-2">';
+//    examHTML += '<button id = "delete-'+id+'" type="button" class="btn btn-danger" onclick = "deleteExam(this)">Delete</button>';
+//    examHTML += '</div>';
+//    examHTML += '</div>';
+//    examHTML += '</div>';
 
-   $('#exams').append(examHTML);
-}
+//    $('#exams').append(examHTML);
+// }
