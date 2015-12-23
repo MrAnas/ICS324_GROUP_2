e57d3d7f0fb76229c52de83365459626b6822f33
Osama Alsalman <thenewosama@gmail.com>
2015-12-23 14:08:23 +0300

    cookie_prep

--- login.php
+++ login.php
@@ -85,7 +85,7 @@
        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
        <!-- Include all compiled plugins (below), or include individual files as needed -->
        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
+       <script src="js/js.cookie.js"></script>
        <script>
 
        function signIn(){
@@ -97,6 +97,7 @@
               });
               } else {
               bootbox.confirm("Welcome to our site", function(result) {
+                 console.log(data);
                  window.open("/user.php");
                }
              );}
